03b198fe4a8db49288f215102d28c8bd91cd625c
RafaZeero <rafaa99@gmail.com>
2023-01-17 17:16:14 -0300

    feat: create exercise 002

---
+++ src/answers/002.dynamic-keys.ts
@@ -0,0 +1,17 @@
+const createCache = () => {
+  const cache: Record<string, string> = {};
+
+  const add = (id: string, value: string) => {
+    cache[id] = value;
+  };
+
+  const remove = (id: string) => {
+    delete cache[id];
+  };
+
+  return {
+    cache,
+    add,
+    remove
+  };
+};
---
+++ src/exercises/002.dynamic-keys.ts
@@ -0,0 +1,19 @@
+const createCache = () => {
+  const cache = {};
+
+  const add = (id: string, value: string) => {
+    cache[id] = value;
+  };
+
+  const remove = (id: string) => {
+    delete cache[id];
+  };
+
+  return {
+    cache,
+    add,
+    remove
+  };
+};
+
+export {};
