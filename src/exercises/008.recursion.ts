f48c46884abf20ffccaae49babebfae6f85be760
RafaZeero <rafaa99@gmail.com>
2023-01-26 22:44:25 -0300

    chore: remove answers folder from proj

--- src/exercises/008.recursion.ts
+++ src/exercises/008.recursion.ts
@@ -26,23 +26,25 @@ type ConvertedNumberNestedObjectType = NumberNestedObject<NestedObjectType>;
 const nested: ConvertedNumberNestedObjectType = {
   prop1: {
     prop2: 3,
-    prop3: 1
+    prop3: 1,
   },
   prop4: {
     prop5: {
-      prop6: 1
-    }
-  }
+      prop6: 1,
+    },
+  },
 };
 
 const nested2: ConvertedNumberNestedObjectType = {
   prop1: {
     prop2: 1,
-    prop3: 4
+    prop3: 4,
   },
   prop4: {
     prop5: {
-      prop6: 3
-    }
-  }
+      prop6: 3,
+    },
+  },
 };
+
+export {};
